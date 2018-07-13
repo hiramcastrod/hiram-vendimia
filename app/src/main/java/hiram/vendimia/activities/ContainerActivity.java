1495ec842f60c3b59fe14cb2675bc37ebb0f06e2
Jorge Hiram <hiram@indqtech.com>
2018-07-12 18:53:38 -0600

    nav bar complete, main layouts, start to code

---
+++ app/src/main/java/hiram/vendimia/activities/ContainerActivity.java
@@ -0,0 +1,15 @@
+package hiram.vendimia.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import hiram.vendimia.R;
+
+public class ContainerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_container);
+    }
+}
