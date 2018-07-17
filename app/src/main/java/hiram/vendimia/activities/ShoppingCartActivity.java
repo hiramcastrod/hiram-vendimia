5f159e39ae1642618c2092772ff6bda910712aec
hiramcastrod <hiramc@live.com.mx>
2018-07-17 00:28:36 -0600

    load months total and pays, some views, add mocky clients

--- app/src/main/java/hiram/vendimia/activities/ShoppingCartActivity.java
+++ app/src/main/java/hiram/vendimia/activities/ShoppingCartActivity.java
@@ -5,6 +5,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import hiram.vendimia.activities.PopUpActivity;
 
@@ -22,14 +24,14 @@ import hiram.vendimia.adapters.ShoppingCartAdapter;
 import hiram.vendimia.models.Cart;
 
 public class ShoppingCartActivity extends AppCompatActivity {
-    TextView tvModel, tvPrice, tvQuantity, tvAmount, total;
+    TextView tvModel, tvPrice, tvQuantity, tvAmount, total, tvThreeM, tvSixM, tvNineM, tvTwelveM;
     ImageView ivIcon;
     RecyclerView cartRecyclerView;
     ArrayList<Cart> cartList;
     float amount;
     double amountDouble;
     RadioGroup radioGroup;
-    public int selectedMonth;
+    Button proceedtoPay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,6 +45,12 @@ public class ShoppingCartActivity extends AppCompatActivity {
         cartRecyclerView = findViewById(R.id.recyclerview_cart);
         total = findViewById(R.id.textview_alltotal_cart);
         radioGroup = findViewById(R.id.radio_months);
+        proceedtoPay = findViewById(R.id.button_accept_cart);
+
+        tvThreeM = findViewById(R.id.tv_threemonths);
+        tvSixM = findViewById(R.id.tv_sixmonths);
+        tvNineM = findViewById(R.id.tv_ninemonths);
+        tvTwelveM = findViewById(R.id.tv_twelvemonths);
 
 
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getApplicationContext());
@@ -51,8 +59,13 @@ public class ShoppingCartActivity extends AppCompatActivity {
         ShoppingCartAdapter cartAdapter = new ShoppingCartAdapter(loadCartList(), R.layout.cart_viewholder, this );
         cartRecyclerView.setAdapter(cartAdapter);
         for(int i = 0; i<loadCartList().size(); i++){
-            amount = amount + cartList.get(i).getTotal();
+            amount = amount + (cartList.get(i).getTotal() * cartList.get(i).getQuantity());
         }
+        tvThreeM.setText("$" + String.valueOf((float)(amount * (1+(2.8*3)/100))/3));
+        tvSixM.setText("$" + String.valueOf((float)(amount * (1+(2.8*6)/100))/6));
+        tvNineM.setText("$" + String.valueOf((float)(amount * (1+(2.8*9)/100))/9));
+        tvTwelveM.setText("$" + String.valueOf((float)(amount * (1+(2.8*12)/100))/12));
+        radioGroup.check(R.id.three_radio);
         radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(RadioGroup group, int checkedId) {
@@ -77,6 +90,13 @@ public class ShoppingCartActivity extends AppCompatActivity {
             }
         });
         total.setText(String.valueOf(amount));
+
+        proceedtoPay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
 
 
     }
